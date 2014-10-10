8a5dfc611abe53038b278e5837d2f1851657ce97
Brian Johnson <brian.johnson@gree.net>
2014-10-10 16:37:27 -0700

    - s/3.4.5/3.4.6/g - Update checksum for 3.4.6

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,7 +3,7 @@
 class zookeeper::config {
   require boxen::config
 
-  $version    = '3.4.5-boxen1'
+  $version    = '3.4.6-boxen1'
 
   $configdir  = "${boxen::config::configdir}/zookeeper"
   $datadir    = "${boxen::config::datadir}/zookeeper"
