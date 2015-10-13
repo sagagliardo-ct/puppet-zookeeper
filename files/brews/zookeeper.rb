658b175787516ae118c64405e8fb0c9b8c824689
sagagliardo-ct <sagagliardo-ct@users.noreply.github.com>
2015-10-13 16:57:36 -0400

    updated zookeeper.rb
    Added latest updates from homebrew brews to work with el-capitan

--- files/brews/zookeeper.rb
+++ files/brews/zookeeper.rb
@@ -2,31 +2,39 @@ require "formula"
 
 class Zookeeper < Formula
   homepage "https://zookeeper.apache.org/"
-  version "3.4.6-boxen3"
+  version "3.4.6-boxen4"
 
   stable do
-    url "http://www.apache.org/dyn/closer.cgi?path=zookeeper/zookeeper-3.4.6/zookeeper-3.4.6.tar.gz"
-    sha1 "2a9e53f5990dfe0965834a525fbcad226bf93474"
+    url "https://www.apache.org/dyn/closer.cgi?path=zookeeper/zookeeper-3.4.6/zookeeper-3.4.6.tar.gz"
+    sha256 "01b3938547cd620dc4c93efe07c0360411f4a66962a70500b163b59014046994"
 
-    # To resolve Yosemite build errors.
+    # To resolve >+Yosemite build errors.
     # https://issues.apache.org/jira/browse/ZOOKEEPER-2049
-    if MacOS.version == :yosemite
+    if MacOS.version >= :yosemite
       patch :p0 do
         url "https://issues.apache.org/jira/secure/attachment/12673210/ZOOKEEPER-2049.noprefix.branch-3.4.patch"
-        sha1 "ff0e971c028050ccebd8cc7caa348ab14716d664"
+        sha256 "b90eda47d21e60655dffe476eb437400afed24b37bbd71e7291faa8ece35c62b"
       end
     end
+  end
+  
+  bottle do
+    cellar :any
+    revision 2
+    sha256 "12241242d81eb12e9d6f520bfd9a20c7a52682bc08435952109fe49e8257b7c3" => :el_capitan
+    sha256 "568d84b98466d842c3a1fa799892e01769bfa06069809fcd2d52f15c40285e50" => :yosemite
+    sha256 "49ae8ae95b5f631b2883f88ca0a91c23aea84c334f018a24734a16e828ca8dbd" => :mavericks
   end
 
   head do
     url "https://svn.apache.org/repos/asf/zookeeper/trunk"
 
-    # To resolve Yosemite build errors.
+    # To resolve >+Yosemite build errors.
     # https://issues.apache.org/jira/browse/ZOOKEEPER-2049
-    if MacOS.version == :yosemite
+    if MacOS.version >= :yosemite
       patch :p0 do
         url "https://issues.apache.org/jira/secure/attachment/12673212/ZOOKEEPER-2049.noprefix.trunk.patch"
-        sha1 "79ed0793e4693c9bbb83aad70582b55012f19eac"
+        sha256 "64b5a4279a159977cbc1a1ab8fe782644f38ed04489b5a294d53aea74c84db89"
       end
     end
 
@@ -37,9 +45,37 @@ class Zookeeper < Formula
     depends_on "automake" => :build
   end
 
-  option "perl", "Build Perl bindings"
+  option "with-perl", "Build Perl bindings"
+  
+  deprecated_option "perl" => "with-perl"
 
   depends_on :python => :optional
+  
+    def shim_script(target)
+    <<-EOS.undent
+      #!/usr/bin/env bash
+      . "#{etc}/zookeeper/defaults"
+      cd "#{libexec}/bin"
+      ./#{target} "$@"
+    EOS
+  end
+
+  def default_zk_env
+    <<-EOS.undent
+      export ZOOCFGDIR="#{etc}/zookeeper"
+    EOS
+  end
+
+  def default_log4j_properties
+    <<-EOS.undent
+      log4j.rootCategory=WARN, zklog
+      log4j.appender.zklog = org.apache.log4j.FileAppender
+      log4j.appender.zklog.File = #{var}/log/zookeeper/zookeeper.log
+      log4j.appender.zklog.Append = true
+      log4j.appender.zklog.layout = org.apache.log4j.PatternLayout
+      log4j.appender.zklog.layout.ConversionPattern = %d{yyyy-MM-dd HH:mm:ss} %c{1} [%p] %m%n
+    EOS
+  end
 
   def install
     # Don't try to build extensions for PPC
@@ -61,17 +97,21 @@ class Zookeeper < Formula
       system "make", "install"
     end
 
-    cd "src/contrib/zkpython" do
-      system "python", "src/python/setup.py", "build"
-      system "python", "src/python/setup.py", "install", "--prefix=#{prefix}"
-    end if build.with? "python"
+    if build.with? "python"
+      cd "src/contrib/zkpython" do
+        system "python", "src/python/setup.py", "build"
+        system "python", "src/python/setup.py", "install", "--prefix=#{prefix}"
+      end
+    end
 
-    cd "src/contrib/zkperl" do
-      system "perl", "Makefile.PL", "PREFIX=#{prefix}",
-                                    "--zookeeper-include=#{include}",
-                                    "--zookeeper-lib=#{lib}"
-      system "make", "install"
-    end if build.include? "perl"
+    if build.with? "perl"
+      cd "src/contrib/zkperl" do
+        system "perl", "Makefile.PL", "PREFIX=#{prefix}",
+                                      "--zookeeper-include=#{include}",
+                                      "--zookeeper-lib=#{lib}"
+        system "make", "install"
+      end
+    end
 
     rm_f Dir["bin/*.cmd"]
 
@@ -81,7 +121,61 @@ class Zookeeper < Formula
     else
       libexec.install Dir["bin", "contrib", "lib", "*.jar"]
     end
+
+    bin.mkpath
+    (etc/"zookeeper").mkpath
+    (var/"log/zookeeper").mkpath
+    (var/"run/zookeeper/data").mkpath
+
+    Pathname.glob("#{libexec}/bin/*.sh") do |path|
+      next if path == libexec+"bin/zkEnv.sh"
+      script_name = path.basename
+      bin_name    = path.basename ".sh"
+      (bin+bin_name).write shim_script(script_name)
+    end
+
+    defaults = etc/"zookeeper/defaults"
+    defaults.write(default_zk_env) unless defaults.exist?
+
+    log4j_properties = etc/"zookeeper/log4j.properties"
+    log4j_properties.write(default_log4j_properties) unless log4j_properties.exist?
+
+    inreplace "conf/zoo_sample.cfg",
+              /^dataDir=.*/, "dataDir=#{var}/run/zookeeper/data"
+    cp "conf/zoo_sample.cfg", "conf/zoo.cfg"
+    (etc/"zookeeper").install ["conf/zoo.cfg", "conf/zoo_sample.cfg"]
   end
 
   plist_options :manual => "zkServer start"
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>EnvironmentVariables</key>
+        <dict>
+           <key>SERVER_JVMFLAGS</key>
+           <string>-Dapple.awt.UIElement=true</string>
+        </dict>
+        <key>KeepAlive</key>
+        <dict>
+          <key>SuccessfulExit</key>
+          <false/>
+        </dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/zkServer</string>
+          <string>start-foreground</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>WorkingDirectory</key>
+        <string>#{var}</string>
+      </dict>
+    </plist>
+    EOS
+  end
 end
